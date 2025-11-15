994eda3e6d0f5e4cd72b1be904ec1b03541b82a0
CHIROOTH <chiroothgirigowda@gmail.com>
2025-11-14 17:34:14 -0700

    removing trail script

--- trail.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# Load the weights
-weights = torch.load('relevance_head.pt', map_location='cpu')
-
-# Print parameter names and their corresponding shapes
-for name, param in weights.items():
-    print(f"Name: {name}, Shape: {param.shape}")
-    print(f"Weights for {name}:\n{param}\n")
